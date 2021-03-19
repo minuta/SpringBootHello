8897d3f5cf16de5db1710334a7f96f49e8d03bc1
AF <gammamustang@gmail.com>
2021-03-19 20:19:55 +0100

    init commit

---
+++ src/test/java/com/example/SpringHi/SpringHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringHi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
